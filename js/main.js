a82ebe22fd3ee4111aa7d0823a7379c9e99f05e8
Nicola Invernizzi <nginvernizzi1@gmail.com>
2020-10-20 14:02:32 +0200

    js done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log('test');
+var numbers = [];
+
+var counter = 1;
+
+while (counter <= 100) {
+    
+    var number = counter;
+
+    if (counter % 3 == 0 && counter % 5 == 0) {
+        number = 'FizzBuzz';
+    } else if (counter % 3 == 0) {
+        number = 'Fizz';
+    } else if (counter % 5 == 0) {
+        number = 'Buzz';
+    }
+    
+    numbers.push(number);
+
+    counter++;
+}
+
+console.log(numbers);
